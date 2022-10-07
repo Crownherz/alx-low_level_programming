ddc3104d385feedfe9b6afacb78ed58bc8485b5b
Adedeji Oriade <deji.oriade@gmail.com>
2022-10-06 19:20:23 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
 		return (NULL);
 	}
